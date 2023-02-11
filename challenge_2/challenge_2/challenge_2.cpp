18d7bc62a52ac37a3854a649290c39b7374f473c
Oscar M <oscarmventura@icloud.com>
2023-02-11 17:04:14 -0600

    displaying the value that change

--- challenge_2/challenge_2/challenge_2.cpp
+++ challenge_2/challenge_2/challenge_2.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <vector>
+
+
 using namespace std;
 
 
@@ -21,16 +23,21 @@ int main()
 
     vector1.push_back(10);
     vector1.push_back(20);
+    cout << "\nThe elements on vector 1 are:" << endl;
     cout << vector1.at(0) << endl;
     cout << vector1.at(1) << endl;
     cout << vector1.size() << endl;
-
+    
+    
+    
     vector2.push_back(100);
     vector2.push_back(200);
+    cout << "\nThe elements on vector 2 are:" << endl;
     cout << vector2.at(0) << endl;
     cout << vector2.at(1) << endl;
     cout << vector2.size() << endl;
-
+    
+   
     //Adding vector1 to vector_2d dynamically
     vector_2d.push_back(vector1);
     vector_2d.push_back(vector2);
@@ -42,18 +49,16 @@ int main()
     cout << vector_2d.at(1).at(0) << endl;
     cout << vector_2d.at(1).at(1) << endl;
    
+    //Change vector1 at(0) to 1000
+    int new_value = 1000;
+    cout << new_value << endl;
+    cin >> vector1.at(0);
 
+    cout << "\nUpdated:" << endl;
+    cout << vector1.at(0) << endl;
 
+    
    
-   
-    
-
-    
-    
-
-
-
-
 
 
     return 0;
