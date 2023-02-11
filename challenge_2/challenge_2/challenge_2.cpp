5ca45559a7ac7b989e2711404268fb190fd00085
Oscar M <oscarmventura@icloud.com>
2023-02-11 16:35:28 -0600

    both vectors are working

--- challenge_2/challenge_2/challenge_2.cpp
+++ challenge_2/challenge_2/challenge_2.cpp
@@ -11,17 +11,26 @@ int main()
 {
     
     vector <int> vector1{};
+    vector <int> vector2{};
+
     vector1.push_back(10);
     vector1.push_back(20);
 
+    vector2.push_back(100);
+    vector2.push_back(200);
+
+   
+    //testing
     cout << vector1.at(0) << endl;
     cout << vector1.at(1) << endl;
-
     cout << vector1.size() << endl;
 
+    cout << vector2.at(0) << endl;
+    cout << vector2.at(1) << endl;
+    cout << vector2.size() << endl;
 
     
-    vector <int> vector2{};
+    
 
 
 
